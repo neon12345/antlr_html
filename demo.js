9d55b86d3676ee902546f7fdfa455499b98c4db2
neon12345 <784389+neon12345@users.noreply.github.com>
2024-02-06 09:19:39 +0100

    Update demo.js
    fix demo

--- demo.js
+++ demo.js
@@ -1,9 +1,5 @@
-var root_funcs = current.queryAll("*:not(Class_declaration) Function_declaration");
-println(root_funcs.length);
+var root_funcs = current.queryAll("Class_body Function_declaration");
+println('number of class mehtods: ' + root_funcs.length);
 root_funcs.forEach(el =>{
-    
-    println(el.query('Function_name').query('Identifier').text());
-    
+    println('mehtod name: ' + el.query('Function_name').query('Identifier').text());
 })
-
-//println(JSON.stringify(current.queryAll('Function_call_argument_list > Function_call_argument:nth-child-of(1)').length))
